383ab9c05c86d7cc0df19d23186dc04358dafb02
IndiraShrestha <indirashrestha.21@gmail.com>
2016-05-02 17:39:16 -0400

    removed console.log & added append

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -1,21 +1,16 @@
 var i = 1;
 for (i = 1; i<101; i++){
-	if (i%3 === 0){
-		$('p.fizzBuzz').append('<p>' 
-			+console.log("Fizz")
-			+'</p>');
-	} else if (i%5 === 0){
-		$('p.fizzBuzz').append('<p>'
-			+console.log("Buzz")
-			+'</p>');
-	} else if ( i%3 && i%5 === 0 ){
-		$('p.fizzBuzz').append('<p>'
-			+console.log("FizzBuzz")
-			+'</p>');
-	} else {
-		$('p.fizzBuzz').append('</p>'
-			+console.log(i)
-			+'</p>');	
+	if (i%3 === 0 && i%5 === 0){
+		$('p.fizzBuzz').append('<p>FizzBuzz</p>');
+	} 
+		else if (i%5 === 0){
+			$('p.fizzBuzz').append('<p>Buzz</p>');
+	} 
+		else if (i%3 === 0 ){
+		$('p.fizzBuzz').append('<p>Fizz</p>');
+	} 
+		else {
+		$('p.fizzBuzz').append('<p>'+i+'</p>');	
 	}
 }
 	
